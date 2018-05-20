0b68af9dbca0ce2944345983628d8bde637729dd
callac <pccindo78@msn.com>
2018-05-21 00:50:37 +0200

    datetimepicker_works

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,27 @@
 class ContactsController < ApplicationController
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contact_path(@contact)
+    else
+      render 'index'
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:appointment)
   end
 end
